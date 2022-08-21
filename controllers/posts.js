b79157102917ac078ed42e1a373de3d82c0c9292
Ross Stephenson-Gill <rossstephensongill@gmail.com>
2022-08-21 19:59:20 +0100

    Removed unecessary api call to send push message

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
+import webpush from "web-push";
 import PostMessage from "../models/postMessage.js";
+import Subscription from "../models/subscription.js";
 
 // https://www.restapitutorial.com/httpstatuscodes.html
 export const getPosts = async (req, res) => {
@@ -22,6 +24,43 @@ export const createPost = async (req, res) => {
 
   try {
     await newPost.save();
+
+    webpush.setVapidDetails(
+      "mailto:rossstephensongill@gmail.com",
+      "BH3ZQxUdEIciu6Kz6Tiq7udXG1LiBMgEELig2eMNQzaJnHbLv8Nd0zbgquA7XP-N6SW8gCXFQWl_9NmK2OP1BRQ", // public key
+      "3YESRcm6ngaRwm2Pbay7GsfNgAmHuMR9FJ4NRAOjWPg" // private key
+    );
+
+    const query = await Subscription.find().lean();
+
+    console.log("Query: ", query);
+
+    for await (const sub of query) {
+      console.log("sub: ", sub);
+      const pushConfig = {
+        endpoint: sub.endpoint,
+        keys: {
+          auth: sub.keys.auth,
+          p256dh: sub.keys.p256dh,
+        },
+      };
+
+      console.log("Push config:", pushConfig);
+
+      webpush
+        .sendNotification(
+          pushConfig,
+          JSON.stringify({
+            title: "New Post",
+            content: "New Post Added!",
+            openUrl: "/",
+          })
+        )
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+
     res.status(201).json(newPost);
   } catch (error) {
     res.status(409).json({ message: error.message });
